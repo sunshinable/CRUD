a651aa54ad09fa58c9ac73b6f945a9e6fd93c3a1
Vereit <sunshine603@internet.ru>
2023-04-10 15:51:15 +0300

    first

---
+++ app.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form');
+const nameInput = document.getElementById('name');
+const phoneInput = document.getElementById('phone');
+const courseInput = document.getElementById('course');
+const table = document.getElementById('table').getElementsByTagName('tbody')[0];
+let data = JSON.parse(localStorage.getItem('data')) || [];
+
+function addRow(item) {
+    const row = table.insertRow();
+    row.insertCell().innerHTML = item.name;
+    row.insertCell().innerHTML = item.phone;
+    row.insertCell().innerHTML = item.course;
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Удалить';
+    deleteButton.addEventListener('click', function() {
+        deleteRow(item);
+    });
+    row.insertCell().appendChild(deleteButton);
+}
+
+function deleteRow(item) {
+    const index = data.findIndex(i => i.name === item.name && i.phone === item.phone && i.course === item.course);
+    if (index > -1) {
+        data.splice(index, 1);
+        localStorage.setItem('data', JSON.stringify(data));
+        table.deleteRow(index);
+    }
+}
+data.forEach(addRow);
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = nameInput.value;
+    const phone = phoneInput.value;
+    const course = courseInput.value;
+    const item = { name, phone, course };
+    data.push(item);
+    localStorage.setItem('data', JSON.stringify(data));
+    addRow(item);
+    form.reset();
+});
